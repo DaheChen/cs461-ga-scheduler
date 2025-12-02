cee6a0ba4bf69ead8800f406bd9236f596818f05
DaheChen <dcgxx@umsystem.edu>
2025-12-02 15:01:54 -0600

    Add main function for SLA Genetic Scheduler

---
+++ main.py
@@ -0,0 +1,8 @@
+# main.py
+
+def main():
+    print("=== SLA Genetic Scheduler ===")
+    print("This is a placeholder main function for CS 461 Program 2.")
+
+if __name__ == "__main__":
+    main()
